40f9fb160615cae9d1609082c17d8d9afb2dfeea
michael-norman-7 <mnorman0801@gmail.com>
2023-01-28 23:15:09 -0600

    added Home page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,26 @@
-
-
 export default function Index() {
   return (
-    <>
-      <p>Hello</p>
-    </>
-  )
+    <div className="row w-75 mx-auto py-5" style={{"marginTop":"6em"}}>
+      <div className="col-6 text-center">
+        <img className="mx-auto" src="/old_lady.jpg"></img>
+      </div>
+      <div className="col-6 d-flex">
+        <div className="my-auto align-middle px-3 fs-5">
+          <p>
+            The well-being of others is always at the forefront of our minds as
+            healthcare providers. [Website Name] is designed to give people
+            readily accessible and easy-to-understand information about their
+            medical diagnoses and medications.
+          </p>
+          <p>
+            We understand that remembering to take medication can be a daunting
+            task, especially for seniors. That's why we've designed our
+            user-friendly platform with simplicity in mind, ensuring that our
+            elderly users can easily set reminders and stay on top of their
+            medication regimen.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }

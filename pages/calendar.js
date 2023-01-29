36a1e8f9ff72f2b3aecb7e346830a13ea56830a7
michael-norman-7 <mnorman0801@gmail.com>
2023-01-28 22:49:19 -0600

    added login and signup pages

--- pages/calendar.js
+++ pages/calendar.js
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from "react";
 export default function calendar() {
   const [dailyList, setDailyList] = useState([]);
   const [checkedState, setCheckedState] = useState([]);
+  const [load, setLoad] = useState(true);
 
   let list = [
     { task: "amoxicillin 40mg", time: "1 pill each morning", taken: 1 },
@@ -19,10 +20,11 @@ export default function calendar() {
   useEffect(() => {
     loadList();
     loadChecks();
-  }, [dailyList, checkedState]);
+  }, [load]);
 
   function loadList() {
     setDailyList(list);
+    setLoad(false);
   }
 
   function loadChecks() {
@@ -31,18 +33,20 @@ export default function calendar() {
       checks.push(day.taken);
     });
     setCheckedState(checks);
-    console.log(dailyList.length);
   }
 
-  function handleCheckChange(index) {
-    let checks = checkedState;
-    checks[index] = !checks[index];
-    setCheckedState(checks);
+  function handleCheckChange(position) {
+    const updatedCheckedState = checkedState.map((item, index) =>
+      index === position ? !item : item
+    );
+    setCheckedState(updatedCheckedState);
   }
 
   return (
     <div className="row m-0">
-      <div className="col-8"><CalendarComponent/></div>
+      <div className="col-8 my-3 px-3">
+        <CalendarComponent />
+      </div>
       <div className="col-4">
         <div className="w-75 m-5">
           <h4 className="w-75">Daily Treatment</h4>

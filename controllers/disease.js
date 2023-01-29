13af8c2576dc457778e2d556c82afe162185bc2f
MainLaptop <40152651+Quivun@users.noreply.github.com>
2023-01-28 20:22:46 -0600

    minor updates

--- controllers/disease.js
+++ controllers/disease.js
@@ -1,17 +1,5 @@
-const mongoose = require("mongoose"); //import mongoose
-
-// disease schema
-const DiseaseSchema = new mongoose.Schema({
-    id: {type:Number, required:true},
-    name: String,
-    causes: String,
-    symptoms: Array,
-    desc: String,
-    start_date: Date,
-    end_date: Date,
-    comments: String,
-    user_id: Number
-});
-
-const Disease = mongoose.model('Disease', DiseaseSchema); //convert to model named Disease
-module.exports = Disease; //export for controller use
+// post disease route
+const newDisease = (req, res, next) => {
+    res.json({message: "POST new disease"});
+};
+module.exports = {newDisease};
--- server.js
+++ server.js
@@ -1,23 +1,13 @@
 require('dotenv').config();
+
 const mongoose = require('mongoose');
 
 //establish connection to database
 mongoose.connect(
     process.env.MONGODB_URI,
-    { useFindAndModify: false, useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true},
+    {useUnifiedTopology: true, useNewUrlParser: true},
     (err) => {
         if (err) return console.log("Error: ", err);
-        console.log("MongoDB Connection -- Ready state is:", mongoose.connection.readyState);
+        console.log("MongoDB Connection : Ready state is:", mongoose.connection.readyState);
     }
 );
-
-var http = require('http');
-
-var server = http.createServer(function(req, res) {
-    res.writeHead(200, { "Content-type": "text/plain" });
-    res.end("Hello world\n");
-});
-
-server.listen(3001, function() {
-    console.log('Server is running at 3001')
-});
